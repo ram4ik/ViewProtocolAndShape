fce9cb8c9674955270f88c95174e1e252a5f9a1e
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-12-30 11:22:10 +0200

    View Protocols and Shapes

---
+++ ViewProtocolAndShape/Helper.swift
@@ -0,0 +1,22 @@
+//
+//  Helper.swift
+//  ViewProtocolAndShape
+//
+//  Created by ramil on 30.12.2020.
+//
+
+import SwiftUI
+
+extension CGContext {
+    static func pdf(size: CGSize, render: (CGContext) -> ()) -> Data {
+        let pdfData = NSMutableData()
+        let consumer = CGDataConsumer(data: pdfData)!
+        var mediaBox = CGRect(origin: .zero, size: size)
+        let pdfContext = CGContext(consumer: consumer, mediaBox: &mediaBox, nil)!
+        pdfContext.beginPage(mediaBox: &mediaBox)
+        render(pdfContext)
+        pdfContext.endPage()
+        pdfContext.closePDF()
+        return pdfData as Data
+    }
+}
