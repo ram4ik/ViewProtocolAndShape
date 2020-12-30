99515708da870759c077d7116aba1178a98863d2
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-12-30 11:05:58 +0200

    Initial Commit

---
+++ ViewProtocolAndShape/ViewProtocolAndShapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewProtocolAndShapeApp.swift
+//  ViewProtocolAndShape
+//
+//  Created by ramil on 30.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct ViewProtocolAndShapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
